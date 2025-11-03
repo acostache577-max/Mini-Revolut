603944ceb997f888c296d2c28e833e8008a54943
acostache577-max <acostache577@gmail.com>
2025-11-03 07:42:42 +0200

    Versiune 2.1.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = 'minirevolut-v1';
 const FILES_TO_CACHE = ['.', 'index.html', 'app.js', 'manifest.json', 'icon-192.svg', 'icon-512.svg'];
 self.addEventListener('install', evt => { evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))); self.skipWaiting(); });
